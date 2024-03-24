610887cd5adaa00c6800838b67d957fff539de58
Matheus Marques <99177444matheus@gmai.com>
2024-03-24 03:39:50 -0300

    correction

--- src/server.mjs
+++ src/server.mjs
@@ -31,7 +31,7 @@ app.post('/users', async (request, reply) => {
 
 app.listen({
     host: '0.0.0.0',
-    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
 }).then(() => {
     console.log('HTTP Server Running')
 })
